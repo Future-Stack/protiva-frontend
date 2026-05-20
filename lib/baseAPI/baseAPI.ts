cb987d6f6e378c1560712be798d58d160b655150
Sahara Islam <sahara@softvence.com>
2026-05-20 12:48:23 +0600

    Resolve Privacy and Provider

--- lib/baseAPI/baseAPI.ts
+++ lib/baseAPI/baseAPI.ts
@@ -2,8 +2,8 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQueryAPI = fetchBaseQuery({
-  baseUrl:'https://1z13k3v3-3000.inc1.devtunnels.ms/',
-  // baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://api.kaajbd.com.bd/',
+  // baseUrl:'https://1z13k3v3-3000.inc1.devtunnels.ms/',
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || 'https://api.kaajbd.com.bd/',
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth?.accessToken;
